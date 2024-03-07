50d7309ff2970718d762f1bb6195cfc9fb578e3e
Pugach-Nazar <qwertyasd0933@gmail.com>
2024-03-07 18:52:52 +0200

    lab 2 done

--- lab1/src/main/java/entity/Entity.java
+++ lab1/src/main/java/entity/Entity.java
@@ -1,36 +1,57 @@
 package entity;
 
-public class Entity {
-  private String title;
-  private int age;
-  private float width;
-  
-  public Entity(String title, int age, float width) {
-    super();
-    this.title = title;
-    this.age = age;
-    this.width = width;
-  }
-  public String getName() {
-    return title;
-  }
-  public void setName(String name) {
-    this.title = name;
-  }
-  public int getAge() {
-    return age;
-  }
-  public void setAge(int age) {
-    this.age = age;
-  }
-  public float getWidth() {
-    return width;
-  }
-  public void setWidth(float width) {
-    this.width = width;
-  }
-  @Override
-  public String toString() {
-    return "{\"title\": \""+title+"\", \"age\":"+age+", \"width\":"+width+"}";
-  }
+import java.io.Serializable;
+
+public class Entity implements Serializable {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	
+	private String title;
+	private int age;
+	private float width;
+	
+	public Entity(String title, int age, float width) {
+		super();
+		this.title = title;
+		this.age = age;
+		this.width = width;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public float getWidth() {
+		return width;
+	}
+
+	public void setWidth(float width) {
+		this.width = width;
+	}
+
+	@Override
+	public String toString() {
+		return "{\"title\": \""+title+"\", \"age\":"+ age+", \"width\":"+width+"}";
+	}
+	
+	
+	
+	
+
 }
--- lab1/src/main/java/servlet/Servlet1.java
+++ lab1/src/main/java/servlet/Servlet1.java
@@ -7,27 +7,43 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-import entity.*;
 
-/**
- * Servlet implementation class Servlet1
- */
+import crud.Lab2CrudInterface;
+import entity.Entity;
+
+
 @WebServlet("/Servlet1")
 public class Servlet1 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	ServletConfigInterface servletConfig;
+	Lab2CrudInterface lab2Crud;
+	
+	
+	public Servlet1() {
+		super();
 		
-		response.setContentType("application/json");
+		this.servletConfig = new ServletConfig();
+		this.lab2Crud =  servletConfig.getCrud();
+	}
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
 		
 		PrintWriter out = response.getWriter();
 		
-		Entity entity = new Entity("Name1", 24 ,2.90f);
-	    out.println("["+entity+"]");
+	    out.println("["+lab2Crud.readEntity()+"]");
 	}
+	
+	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		String title = request.getParameter("title");
+		int age = Integer.parseInt(request.getParameter("age"));
+		float width = Float.parseFloat(request.getParameter("width"));
+		
+		lab2Crud.updateEntity(new Entity(title,age,width));
+		
+	}
+
 
 }
